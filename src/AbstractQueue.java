0b174a1d483542c8a4689a599850a4a46387f0dc
AKichiN73 <89227223902k@gmail.com>
2020-11-29 19:18:39 +0300

    Initial commit

---
+++ src/AbstractQueue.java
@@ -0,0 +1,14 @@
+abstract public class AbstractQueue implements Queue {
+
+    protected int size;
+    protected int head;
+    protected int tail;
+
+    abstract public void enqueue(Object element);
+    abstract public Object element();
+    abstract public Object dequeue();
+    abstract public void clear();
+    abstract public int size();
+    abstract public boolean isEmpty();
+
+}
